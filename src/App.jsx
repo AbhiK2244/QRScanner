1d78039566b43b3780f0f9ddbfe818e02c529f26
Abhishek Kamati <kamtiabhi4@gmail.com>
2024-12-10 01:04:38 +0530

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import QRScanner from './QrScanner'
 
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allow access from other devices
-    port: 3000, // The port your app will run on
-  },
 })
